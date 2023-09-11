831ddcdede1a6cb7f2588bf9a979563f3c6a1400
Evgenyy-hud <58087426+Evgenyy-hud@users.noreply.github.com>
2023-09-11 23:53:18 +0300

    Delete smarthome directory

--- smarthome/src/main/java/com/smarthomeweb/smarthome/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smarthomeweb.smarthome.repository;
-
-
-
-import com.smarthomeweb.smarthome.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
